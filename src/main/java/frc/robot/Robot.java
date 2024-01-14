641bdee5f9a0e51b309b1e3061fde4edc0f73039
Jack Cammarata <jackcammarata1@gmail.com>
2024-01-14 14:59:44 -0500

    controls, dependency weirdness

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,73 +1,159 @@
 // Copyright (c) FIRST and other WPILib contributors.
+
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
+//import com.pathplanner.lib.path.PathPlannerServer;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.drive.SwerveDrive;
 
+/**
+ * The VM is configured to automatically run this class, and to call the methods
+ * corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  private RobotContainer robotContainer;
+  private Command autonmousRoutine;
 
-  private RobotContainer m_robotContainer;
-
+  /**
+   * This method is run when the robot is first started up and should be used for
+   * any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
-    m_robotContainer = new RobotContainer();
+    DriverStation.silenceJoystickConnectionWarning(true);
+    //PathPlannerServer.startServer(5811);
+
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our autonomous chooser on the dashboard.
+    robotContainer = RobotContainer.getInstance();
+
+    // for debugging
+    CommandScheduler.getInstance().onCommandInitialize((command) -> System.out.println(command.getName() + " starting..."));
+    CommandScheduler.getInstance().onCommandInterrupt((command) -> System.out.println(command.getName() + " interrupted!"));
+    CommandScheduler.getInstance().onCommandFinish((command) -> System.out.println(command.getName() + " ended."));
   }
 
+  /**
+   * This method is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>
+   * This runs after the mode specific periodic methods, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
   @Override
   public void robotPeriodic() {
+
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
+    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
+  /** This method is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    SwerveDrive.kMaxAngularSpeedRadiansPerSecond = 2 * Math.PI;
 
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
   }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void disabledPeriodic() {
 
+
+    RobotContainer.swerveDrive.isCalibrated = true;
+  }
+
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
-  public void autonomousExit() {}
+  public void autonomousInit() {
+    SwerveDrive.kMaxAngularSpeedRadiansPerSecond = Math.PI / 4;
+    // get the auto from the chooser
+    /*
+    var auto = robotContainer.getAutoRoutine();
+
+    // only run autos that actually have a trajectory to avoid a runtime exception
+    if (auto.isPathPlannerRoutine() && !auto.pathplannerRoutine.isEmpty()) {
+      this.autonmousRoutine = RobotContainer.swerveAutoBuilder.fullAuto(auto.pathplannerRoutine);
+    } else {
+      var startingPose = auto.startingPose != null ? auto.startingPose : RobotContainer.swerveDrive.getPose();
+      RobotContainer.swerveDrive.setPose(startingPose);
+      this.autonmousRoutine = auto.cmdRoutine;
+    }
+
+    // start the auto, if there is one
+    if (this.autonmousRoutine != null) {
+      this.autonmousRoutine.schedule();
+    }
+    */
+  }
+
+  /** This method is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    if (!RobotContainer.swerveDrive.isCalibrated) {
+      DriverStation.reportError("SWERVE DRIVE NOT CALIBRATED!!!!!", false);
+    }
+  }
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+
+    SwerveDrive.kMaxAngularSpeedRadiansPerSecond = 2 * Math.PI;
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (autonmousRoutine != null) {
+      autonmousRoutine.cancel();
     }
+
   }
 
+  /** This method is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
+    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
+  /** This method is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
+  /** This method is called once when the robot is first started up. */
   @Override
-  public void testExit() {}
+  public void simulationInit() {
+  }
+
+  /** This method is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {
+  }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,4 +1,5 @@
 // Copyright (c) FIRST and other WPILib contributors.
+
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -6,20 +7,137 @@ package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.SerialPort.Port;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import frc.robot.commands.DriveSwerveWithXbox;
+import frc.robot.subsystems.drive.SwerveDrive;
 
 public class RobotContainer {
-  public static final AHRS navx = new AHRS(Port.kMXP);
+  private static RobotContainer instance;
 
-  public RobotContainer() {
-    configureBindings();
+  public static RobotContainer getInstance() {
+    if (instance == null) {
+      instance = new RobotContainer();
+    }
+    return instance;
   }
 
-  private void configureBindings() {}
-
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+  /*
+  public AutoRoutine getAutoRoutine() {
+    return autoChooser.getSelected();
   }
+  */
+
+  public static final AHRS navx = new AHRS(Port.kUSB);
+
+  /**
+   * Returns the angle or "yaw" of the robot in degrees. CW positive ranging from
+   * [-180, 180].
+   */
+  public static double getRobotYaw() {
+    return navx.getYaw();
+  }
+
+  /**
+   * Returns the pitch angle of the robot in degrees. This tracks the
+   * forwards/backwards tilt of the robot.
+   */
+  public static double getRobotPitch() {
+    return navx.getRoll();
+  }
+
+  public static double getRobotRoll() {
+    return -navx.getPitch();
+  }
+
+  public static final Field2d field = new Field2d();
+  public static final PowerDistribution pdp = new PowerDistribution();
+
+  // Human Interface Devices (HIDs)
+  public static final CommandXboxController driverController = new CommandXboxController(RobotMap.XBOX_PORT);
+  //public static final OperatorBox operatorBox = new OperatorBox(RobotMap.SWITCHBOX_PORT);
+  //public static final OperatorStick operatorStick = new OperatorStick(RobotMap.JOYSTICK_PORT);
+
+  // Subsystems
+  public static final SwerveDrive swerveDrive = new SwerveDrive();
+
+  //public static final OldLEDStrip leds = new OldLEDStrip();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  private RobotContainer() {
+    this.configureSubsystemDefaultCommands();
+    this.configureBindings();
+  }
+
+  /** Configures the subsystem's default commands. */
+  private void configureSubsystemDefaultCommands() {
+    swerveDrive.setDefaultCommand(new DriveSwerveWithXbox());
+    // arm.setDefaultCommand(new ArmHoldSetpoint());
+    // wrist.setDefaultCommand(new WristHoldSetpoint());
+  }
+
+  private void configureBindings() {
+    this.configureAutomatedBindings();
+    this.configureXboxControllerBindings();
+    this.configureOperatorBoxBindings();
+    this.configureOperatorStickBindings();
+  }
+
+  /** Automated bindings that happen without pressing any buttons. */
+  private void configureAutomatedBindings() {
+    /*
+    Trigger autoCalibrateTeleop = new Trigger(
+        () -> (!swerveDrive.isCalibrated && RobotState.isTeleop() && RobotState.isEnabled()));
+    autoCalibrateTeleop.onTrue(new CenterSwerveModules(true));
+    */
+
+    // Trigger autoPullIn = new Trigger(() -> operatorBox.cubeMode() &&
+    // grabber.getSensor());
+    // autoPullIn.onTrue(new AutoPullIn());
+  }
+
+  /** Binds commands to xbox controller buttons. */
+  private void configureXboxControllerBindings() {
+    //Trigger forceCalibrate = driverController.back();
+    //forceCalibrate.onTrue(new CenterSwerveModules(true));
+
+    // Trigger resetNavx = driverController.start();
+    // resetNavx.onTrue(new InstantCommand(() -> swerveDrive.resetNavx()));
+
+    Trigger resetOdometry = driverController.start();
+    resetOdometry.onTrue(new InstantCommand(() -> swerveDrive.resetOdometry()));
+
+    Trigger slowDrive = driverController.leftTrigger(0.3);
+    slowDrive.onTrue(new InstantCommand(() -> {
+      SwerveDrive.kMaxAngularSpeedRadiansPerSecond = 0.75;
+      SwerveDrive.kMaxSpeedMetersPerSecond = Math.PI / 2.0;
+    }));
+    slowDrive.onFalse(new InstantCommand(() -> {
+      SwerveDrive.kMaxAngularSpeedRadiansPerSecond = 2 * Math.PI;
+      SwerveDrive.kMaxSpeedMetersPerSecond = 3.5;
+    }));
+  }
+
+  /** Binds commands to the operator box. */
+  private void configureOperatorBoxBindings() {
+
+  }
+
+  /** Binds commands to the operator stick. */
+  private void configureOperatorStickBindings() {
+
+  }
+
+  /**
+   * Configures the autonomous chooser over Network Tables (e.g. Smart Dashboard).
+   */
 }
