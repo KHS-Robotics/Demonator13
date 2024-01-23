5bc2c347acd9886998a043771b2aa88843c596b0
JBernberg <josh@scottbernberg.com>
2024-01-23 17:02:59 -0500

    changed math for calculating pos and vel

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,8 +6,11 @@ import edu.wpi.first.math.geometry.Transform3d;
 public class Constants {
   public static final double SENS = 0.5;
 
-  public static final double DRIVE_VEL_ENCODER = 0.000637;
-  public static final double DRIVE_POS_ENCODER = 0.000637;
+  public static final double WHEEL_RADIUS_METERS = 0.0508;
+  public static final double SDS_L2_DRIVE_GEARING = 6.75;
+  public static final double DRIVE_POS_ENCODER = (2 * Math.PI * WHEEL_RADIUS_METERS) / SDS_L2_DRIVE_GEARING;
+  public static final double DRIVE_VEL_ENCODER = DRIVE_POS_ENCODER / 60.0;
+
   public static final double PIVOT_P = 0.01;
   public static final double PIVOT_I = 0.0;
   public static final double PIVOT_D = 0.0001;
