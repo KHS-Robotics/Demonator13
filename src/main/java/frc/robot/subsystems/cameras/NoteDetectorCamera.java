709c5bcc5b93fc199775793ae53f92c9e77d60c7
jackc <jackcammarata1@gmail.com>
2024-01-31 12:15:28 -0500

    miserable multi note logic, too many for loops

--- src/main/java/frc/robot/subsystems/cameras/NoteDetectorCamera.java
+++ src/main/java/frc/robot/subsystems/cameras/NoteDetectorCamera.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj2.command.NotifierCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 
@@ -85,32 +86,57 @@ public class NoteDetectorCamera extends SubsystemBase {
 
   @Override
   public void periodic() {
+    // if any targets are a note that already exists, update that note
+    // if none are, make a new note
+    // if a note is in fov and a target does not update that note's pose in this tick, remove the note
+
+    // this is god awful if anyone has a better way to do all this please change it
+
+    ArrayList<Note> newNotes = new ArrayList<>();
+    ArrayList<Note> notesToRemove = new ArrayList<>();
+    ArrayList<Note> notesInFrame = new ArrayList<>();
+    ArrayList<Translation2d> detectedNotes = new ArrayList<>();
+    List<Pose2d> allNotePoses = new ArrayList<>();
+
+
+    // create list of notes that should be in frame
+    for (Note n : notes) {
+      if (n.isInFov(RobotContainer.swerveDrive.getPose())) {
+        notesInFrame.add(n);
+      }
+    }
+
+    // create list of positions for notes that are actually in frame
     for (PhotonTrackedTarget t : getTargets()) {
-      Translation2d notePose = estimateNotePose(t);
+      detectedNotes.add(estimateNotePose(t));
+    }
 
-      boolean newNote = true;
-
-      if (notes.isEmpty()) {
-        newNote = true;
-      } else {
-        for (Note n : notes) {
-          newNote = !n.addPose(notePose);
-          if (newNote = false) {
-            break;
-          }
+    // if any notes that should be in frame don't get updated, add them to the list of notes to be removed
+    // the implication is that the robot can't see it when it should, so it's probably not there
+    for (Note n : notesInFrame) {
+      boolean hasBeenUpdated = false;
+      // if any note gets updated, set hasBeenUpdated to true, and stop looping (can only update one note)
+      for (Translation2d newPose : detectedNotes) {
+        // if a detected note is new, add it to newNotes, if it is not new, it updates the note that it actually is
+        if (n.addPose(newPose)) {
+          hasBeenUpdated = true;
+          break;
+        } else {
+          newNotes.add(new Note(newPose));
         }
       }
 
-      if (newNote) {
-        notes.add(new Note(notePose));
+      // update remove list
+      if (!hasBeenUpdated) {
+        notesToRemove.add(n);
       }
-
-
-
-      notes.get(0).addPose(notePose);
     }
 
-    List<Pose2d> allNotePoses = new ArrayList<>();
+    // updates note list based on remove/new lists
+    notes.removeAll(notesToRemove);
+    notes.addAll(newNotes);
+
+    // get notes as Pose2ds because glass takes in Pose2d list
     for (Note n : notes) {
       allNotePoses.add(new Pose2d(n.position, new Rotation2d()));
     }
