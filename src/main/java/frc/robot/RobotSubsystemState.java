3d9cff3faeccb9f604ff84f39fdd9296583443f2
jackc <jackcammarata1@gmail.com>
2024-02-27 08:23:48 -0500

    RobotSubsystemState

---
+++ src/main/java/frc/robot/RobotSubsystemState.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import frc.robot.subsystems.Arm.ArmState;
+import frc.robot.subsystems.Intake.IntakeState;
+import frc.robot.subsystems.Shooter.ShooterState;
+
+public enum RobotSubsystemState {
+  kHandoff(IntakeState.kDown, ArmState.kIntake, ShooterState.kIntake),
+  kAmp(IntakeState.kDown, ArmState.kAmp, ShooterState.kAmp),
+  kShoot(IntakeState.kDown, ArmState.kShoot, ShooterState.kShoot),
+  kTravel(IntakeState.kMid, ArmState.kStow, ShooterState.kShoot);
+
+  public final IntakeState intakeState;
+  public final ArmState armState;
+  public final ShooterState shooterState;
+
+  RobotSubsystemState(IntakeState intakeState, ArmState armState, ShooterState shooterState) {
+    this.intakeState = intakeState;
+    this.armState = armState;
+    this.shooterState = shooterState;
+  }
+}
